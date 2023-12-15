cf34a1acbb52bbfcefef299221593911b9d6841c
darko <darkonikoloc@gmail.com>
2023-12-15 18:22:09 +0100

    demo up

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import type { DefineComponent } from 'vue';
+import Test from './components/Test.vue'
+import { Slideshow } from 'vue-slideshow-library';
+</script>
+
+<template>
+  <div
+    style="
+      z-index:20;
+      width: 100%;
+      height: 1000px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 5rem;
+      color: white;
+      background-color: red;
+    ">
+      BEFORE
+  </div>
+  <Slideshow :slides="[
+      {
+        component: Test as DefineComponent,
+        props: {
+          title: 'SLIDE 1'
+        },
+        backgroundColor: 'green',
+        transitionName: 'left'
+      },
+      {
+        component: Test as DefineComponent,
+        props: {
+          title: 'SLIDE 2'
+        },
+        backgroundColor: 'blue',
+        transitionName: 'left'
+      },
+      {
+        component: Test as DefineComponent,
+        props: {
+          title: 'SLIDE 3'
+        },
+        backgroundColor: 'violet',
+        transitionName: 'left'
+      },
+      {
+        component: Test as DefineComponent,
+        props: {
+          title: 'SLIDE 4'
+        },
+        backgroundColor: 'red',
+        transitionName: 'left'
+      }
+    ]"
+    :background-color-transition-duration-in-miliseconds="1000"
+    :background-color-transition-mode="'ease-in'" 
+  />
+  <div
+    style="
+      z-index:20;
+      width: 100%;
+      height: 1000px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 5rem;
+      color: black;
+      background-color: yellow;
+    ">
+      AFTER
+  </div>
+</template>
+
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const props = defineProps(['title'])
+</script>
+
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<style scoped>
+  div {
+    width: 100%;
+    height: 100dvh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 3rem;
+  }
+</style>
